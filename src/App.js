5096fbc7322eaca0c24e1302883931bd47f79390
Ariel SAULNIER <asaulnier@planetworkint.com>
2022-09-22 10:55:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Hero from './Components/Hero';
+import Features from './Components/Features';
+import CTA from './Components/CTA';
+import Pricing from './Components/Pricing';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Stats from './Components/Stats';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero></Hero>
+      <Stats></Stats>
+      <Features></Features>
+     
+      <Pricing></Pricing>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
